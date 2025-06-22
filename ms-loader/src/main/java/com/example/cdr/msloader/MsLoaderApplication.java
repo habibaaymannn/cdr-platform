f21d6ac3ccb1ccb9c3654ddb60a0125f6cdeb9fd
habiba <habibaayman100205@gmail.com>
2025-06-22 10:28:30 +0300

    loader

--- ms-loader/src/main/java/com/example/cdr/msloader/MsLoaderApplication.java
+++ ms-loader/src/main/java/com/example/cdr/msloader/MsLoaderApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class MsLoaderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsLoaderApplication.class, args);
     }
